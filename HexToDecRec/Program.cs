018896168ec047224d6674f44fc76e871b2316c7
Marius Ancutescu <marius.ancutescu@complyadvantage.com>
2020-03-09 18:05:41 +0200

    Add project files.

---
+++ HexToDecRec/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HexToDecRec
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Recursive("12f"));
+        }
+
+		private static int Recursive(string hexNumber)
+		{
+			int deci = 0;
+			String hexCode = "0123456789ABCDEF";
+			hexNumber = hexNumber.ToUpper();
+			int length = hexNumber.Length;
+			if (length > 0)
+			{
+				char ch = hexNumber[0];
+				int digit = hexCode.IndexOf(ch);
+				String substring = hexNumber.Substring(1);
+				deci = digit * (int)Math.Pow(16, length - 1) + Recursive(substring);
+			}
+			return deci;
+		}
+	}
+}
